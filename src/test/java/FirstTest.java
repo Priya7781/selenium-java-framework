a466f9be900a1c6bb3cadfce2d50e69c1aca95a0
Priyadarshi Nayak <priyadarshinayak2001@gmail.com>
2026-05-26 22:53:32 +0530

    added selenium.yml

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,48 +1,74 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class FirstTest {
 
     @Test
     public void launchLoginPage() {
 
-        WebDriver driver = new ChromeDriver();
+        // Chrome options for CI/CD
+        ChromeOptions options = new ChromeOptions();
 
-        // Open URL
-        driver.get("https://priya7781.github.io/login-page/");
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
 
-        // Verify URL contains login
-        Assert.assertTrue(driver.getCurrentUrl().contains("login"));
+        WebDriver driver = new ChromeDriver(options);
 
-        // Enter username
-        driver.findElement(By.xpath("//input[@id='username']"))
-                .sendKeys("admin");
+        try {
 
-        // Enter password
-        driver.findElement(By.xpath("//input[@id='password']"))
-                .sendKeys("1234");
+            // Explicit wait
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        // Click login button
-        driver.findElement(By.xpath("//button[@type='submit']"))
-                .click();
+            // Open URL
+            driver.get("https://priya7781.github.io/login-page/");
 
-        // Fetch success message
-        String actualMessage = driver.findElement(By.xpath("//p[@id='message']"))
-                                     .getText();
+            // Verify URL contains login
+            Assert.assertTrue(driver.getCurrentUrl().contains("login"));
 
-        System.out.println("Actual Message: " + actualMessage);
+            // Enter username
+            driver.findElement(By.xpath("//input[@id='username']"))
+                    .sendKeys("admin");
 
-        // Verify success message
-        Assert.assertEquals(actualMessage, "Login Successful!");
+            // Enter password
+            driver.findElement(By.xpath("//input[@id='password']"))
+                    .sendKeys("1234");
 
-        driver.quit();
+            // Click login button
+            driver.findElement(By.xpath("//button[@type='submit']"))
+                    .click();
+
+            // Wait for success message
+            WebElement messageElement = wait.until(
+                    ExpectedConditions.visibilityOfElementLocated(
+                            By.xpath("//p[@id='message']")
+                    )
+            );
+
+            // Fetch text
+            String actualMessage = messageElement.getText();
+
+            System.out.println("Actual Message: " + actualMessage);
+
+            // Assertion
+            Assert.assertEquals(actualMessage, "Login Successful!");
+
+        } finally {
+
+            // Close browser
+            driver.quit();
+        }
     }
 }
-
-
-
-   
